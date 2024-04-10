689f0581a8fc48291ab440c4fc879206187ae6ac
lucasarg2023 <lopezlucasnqn@gmail.com>
2024-04-10 01:04:01 -0300

    entrega de tp

---
+++ tpentregable.php
@@ -0,0 +1,163 @@
+<?php
+
+/*
+La empresa de Transporte de Pasajeros “Viaje Feliz” quiere registrar la información referente a sus viajes. De cada viaje se precisa almacenar 
+el código del mismo, destino, cantidad máxima de pasajeros y los pasajeros del viaje.
+
+Realice la implementación de la clase Viaje e implemente los métodos necesarios para modificar los atributos de dicha clase (incluso los datos de los pasajeros). 
+Utilice clases y arreglos  para   almacenar la información correspondiente a los pasajeros. Cada pasajero guarda  su “nombre”, “apellido” y “numero de documento”.
+Implementar un script testViaje.php que cree una instancia de la clase Viaje y presente un menú que permita cargar la información del viaje, modificar y ver sus datos.
+
+Modificar la clase Viaje para que ahora los pasajeros sean un objeto que tenga los atributos nombre, apellido, numero de documento y teléfono. El viaje ahora contiene una
+ referencia a una colección de objetos de la clase Pasajero. También se desea guardar la información de la persona responsable de realizar el viaje, para 
+ ello cree una clase ResponsableV que registre el número de empleado, número de licencia, nombre y apellido. La clase Viaje debe hacer referencia al responsable 
+ de realizar el viaje.
+
+Implementar las operaciones que permiten modificar el nombre, apellido y teléfono de un pasajero. Luego implementar la operación 
+que agrega los pasajeros al viaje, solicitando por consola la información de los mismos. Se debe verificar que el pasajero no este cargado mas
+ de una vez en el viaje. De la misma forma cargue la información del responsable del viaje.
+*/
+
+
+class Pasajero {
+    private $nombre;
+    private $apellido;
+    private $numeroDocumento;
+    private $telefono;
+
+    public function __construct($nombre, $apellido, $numeroDocumento, $telefono) {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->numeroDocumento = $numeroDocumento;
+        $this->telefono = $telefono;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getApellido() {
+        return $this->apellido;
+    }
+
+    public function getNumeroDocumento() {
+        return $this->numeroDocumento;
+    }
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function setApellido($apellido) {
+        $this->apellido = $apellido;
+    }
+
+    public function setNumeroDocumento($numeroDocumento) {
+        $this->numeroDocumento = $numeroDocumento;
+    }
+
+
+}
+
+class ResponsableV {
+    private $numeroEmpleado;
+    private $numeroLicencia;
+    private $nombre;
+    private $apellido;
+
+    public function __construct($numeroEmpleado, $numeroLicencia, $nombre, $apellido) {
+        $this->numeroEmpleado = $numeroEmpleado;
+        $this->numeroLicencia = $numeroLicencia;
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+    }
+
+    public function getNumeroEmpleado() {
+        return $this->numeroEmpleado;
+    }
+
+    public function setNumeroEmpleado($numeroEmpleado) {
+        $this->numeroEmpleado = $numeroEmpleado;
+    }
+
+    public function getNumeroLicencia() {
+        return $this->numeroLicencia;
+    }
+
+    public function setNumeroLicencia($numeroLicencia) {
+        $this->numeroLicencia = $numeroLicencia;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function getApellido() {
+        return $this->apellido;
+    }
+
+    public function setApellido($apellido) {
+        $this->apellido = $apellido;
+    }
+
+}
+
+class Viaje {
+    private $codigo;
+    private $destino;
+    private $maxPasajeros;
+    private $pasajeros;
+    private $responsable;
+
+    public function __construct($codigo, $destino, $maxPasajeros, ResponsableV $responsable) {
+        $this->codigo = $codigo;
+        $this->destino = $destino;
+        $this->maxPasajeros = $maxPasajeros;
+        $this->pasajeros = array();
+        $this->responsable = $responsable;
+
+        
+    }
+    public function getCodigo() {
+        return $this->codigo;
+    }
+
+    public function getDestino() {
+        return $this->destino;
+    }
+
+    public function getMaxPasajeros() {
+        return $this->maxPasajeros;
+    }
+
+    public function getPasajeros() {
+        return $this->pasajeros;
+    }
+
+    public function setCodigo($codigo) {
+        $this->codigo = $codigo;
+    }
+
+    public function setDestino($destino) {
+        $this->destino = $destino;
+    }
+
+    public function setMaxPasajeros($maxPasajeros) {
+        $this->maxPasajeros = $maxPasajeros;
+    }
+    // Método para agregar un pasajero al viaje
+    public function agregarPasajero(Pasajero $pasajero) {
+        if (count($this->pasajeros) < $this->setMaxPasajeros()) {
+            $this->pasajeros[] = $pasajero;
+            return true; // Se agregó el pasajero con éxito
+        } else {
+            return false; // El viaje está completo, no se puede agregar más pasajeros
+        }
+    }
+}
+
+   
